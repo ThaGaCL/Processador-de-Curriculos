63c9562e2d8e557e66d15cc78afbab9b657939f4
Thales Carvalho <tgcl21@inf.ufpr.br>
2022-11-07 12:57:02 -0300

    aloca memoria
    aloca memoria para o arquivo

--- libdir.c
+++ libdir.c
@@ -1,81 +1,75 @@
 #include "libdir.h"
 
-FILE* openFile(char* filename)
+FILE *openFile(char *filename)
 {
-    FILE* arq = fopen ("./curriculos/curriculo.xml" , "r") ;
-    if ( ! arq )
+    FILE *arq = fopen("./curriculos/curriculo.xml", "r");
+    if (!arq)
     {
-            perror ("Erro ao abrir arquivo") ;
-            exit (1) ;
+        perror("Erro ao abrir arquivo");
+        exit(1);
     }
     return arq;
 }
 
-int getSize(FILE* file)
+int getSize(FILE *file)
 {
-  
+
     fseek(file, 0, SEEK_END);
-  
+
     int size = ftell(file);
-  
-    fclose(file);
-  
+    rewind(file);
+
     return size;
-
 }
 
-void printFile(FILE* arq)
+void printFile(FILE *arq)
 {
-    
-    char line[LINESIZE+1] ;
+    int fileSize = getSize(arq);
+    char line[LINESIZE + 1];
 
-    while(!feof(arq))
+    char *fileContent = (char *)malloc(fileSize * sizeof(char));
+    fread(fileContent, sizeof(char), fileSize, arq);
+
+    printf("%s", fileContent);
+}
+
+DIR *openDir(char *dirname)
+{
+
+    DIR *dirstream = opendir("./curriculos");
+    if (!dirstream)
     {
-        printf ("%s", line) ;
-        fgets (line, LINESIZE, arq) ;   /*tenta ler a próxima linha*/
+        perror("Couldn't open the directory");
+        exit(1);
     }
 
-}
-
-DIR* openDir(char* dirname)
-{
-    
-   DIR* dirstream = opendir ("./curriculos");
-   if ( ! dirstream )
-   {
-      perror ("Couldn't open the directory");
-      exit (1) ;
-   }
- 
     return dirstream;
-}  
+}
 
-void readDir(DIR* dirstream)
+void readDir(DIR *dirstream)
 {
-  struct dirent *direntry;
-  char* dirname = "curriculo/";
+    struct dirent *direntry;
+    char *dirname = "curriculo/";
 
-  for (;;)
-  {
-    // pega a próxima entrada
-    direntry = readdir (dirstream) ;
-
-    // se for nula, encerra a varredura
-    if (! direntry)
-      break ;
- 
-    if(direntry->d_type == DT_REG)
+    for (;;)
     {
-        
-        printf("%s\n", direntry->d_name);
-        char* path = malloc(sizeof(direntry->d_name)+sizeof(dirname)+2);
-        path = realpath(path,NULL);
-        FILE *arq = openFile(path);
-        printFile(arq);
+        // pega a próxima entrada
+        direntry = readdir(dirstream);
 
-        fclose(arq);
+        // se for nula, encerra a varredura
+        if (!direntry)
+            break;
+
+        if (direntry->d_type == DT_REG)
+        {
+
+            printf("%s\n", direntry->d_name);
+            char *path = malloc(sizeof(direntry->d_name) + sizeof(dirname) + 2);
+            path = realpath(path, NULL);
+            FILE *arq = openFile(path);
+            printFile(arq);
+
+            fclose(arq);
+        }
     }
-
-   }
-   
 }
